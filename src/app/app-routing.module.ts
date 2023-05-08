20d6816d901fb352947354669b38a826404da669
arno67000 <arno68.as@gmail.com>
2023-05-08 16:03:17 +0200

    :tada: basic setup for the new portfolio version + routing + basic header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { PortfolioComponent } from "./components/portfolio/portfolio.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { CvComponent } from "./components/cv/cv.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "portfolio", component: PortfolioComponent },
+    { path: "contact", component: ContactComponent },
+    { path: "cv", component: CvComponent },
+    { path: "**", component: HomeComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { HomeComponent } from "./components/home/home.component";
+import { CvComponent } from "./components/cv/cv.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { PortfolioComponent } from "./components/portfolio/portfolio.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HomeComponent, CvComponent, ContactComponent, PortfolioComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatIconModule,
+        MatButtonModule,
+        MatToolbarModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

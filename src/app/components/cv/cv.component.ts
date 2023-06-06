beaee556dbbe7fadc70ed0f8792e7d9cc98f007c
astutz <arnaud.stutz@neomanis.io>
2023-06-06 11:30:53 +0200

    :sparkles: implement cv page

--- src/app/components/cv/cv.component.ts
+++ src/app/components/cv/cv.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { Dev } from "../../models/dev";
+import { DevService } from "../../services/dev.service";
+import { scaleX } from "../../animations/scales";
+import { listBorderFadeIn, listFadeIn } from "../../animations/fades";
 
 @Component({
-  selector: 'app-cv',
-  templateUrl: './cv.component.html',
-  styleUrls: ['./cv.component.scss']
+    selector: "app-cv",
+    templateUrl: "./cv.component.html",
+    styleUrls: ["./cv.component.scss"],
+    animations: [
+        scaleX("cv__center", "1s 0.2s", 100),
+        listFadeIn("terminal", "0.5s 1s", 25),
+        listBorderFadeIn("bordered", "0.5s 2s", 100),
+    ],
 })
-export class CvComponent implements OnInit {
+export class CvComponent implements OnInit, OnDestroy {
+    @HostBinding("@scaleX")
+    @HostBinding("@listFadeIn")
+    @HostBinding("@listBorderFadeIn")
+    private subscription: Subscription;
+    public dev: Dev | undefined;
+    constructor(private devService: DevService) {
+        this.subscription = this.devService.devSubject.subscribe((dev) => (this.dev = dev));
+        this.devService.emit();
+    }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        return;
+    }
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
 }
